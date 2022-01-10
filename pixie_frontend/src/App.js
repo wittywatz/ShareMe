17ef80eb874d79fa8c5ac4450b0f83d1ffa45f86
wittywatz <watson.agbramu@neofinancial.com>
2022-01-10 14:35:14 -0500

    Pixie_frontend

---
+++ pixie_frontend/src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+import { Login } from './components';
+import Home from './container/Home';
+
+const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let userInfo = localStorage.getItem('user') !== 'undefined';
+
+    if (userInfo) {
+      userInfo = JSON.parse(localStorage.getItem('user'));
+    } else {
+      userInfo = localStorage.clear();
+    }
+
+    if (!userInfo) navigate('/login');
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
+};
+
+export default App;
